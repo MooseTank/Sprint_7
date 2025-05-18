90d877caeaa974714f2a3ccc0d71d236e429f8bc
Pavel Maslov <yessoo@yandex.ru>
2025-05-18 15:01:12 +0300

    First

---
+++ urls.py
@@ -0,0 +1,5 @@
+class Urls:
+    URL_BASIC = 'https://qa-scooter.praktikum-services.ru/'
+    URL_COURIER_CREATE = f'{URL_BASIC}api/v1/courier/'
+    URL_COURIER_LOGIN = f'{URL_BASIC}api/v1/courier/login'
+    URL_ORDERS_CREATE = f'{URL_BASIC}api/v1/orders'
